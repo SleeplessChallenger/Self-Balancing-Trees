d46f5f9841bcf1ff611a2267f81d1e883b35c30f
Daniil Slobodeniuk <sausernsunny@gmail.com>
2022-02-23 20:58:28 +0300

    First commit

---
+++ AVL_trees/AVL.md
@@ -0,0 +1,192 @@
+<h2>AVL trees notes</h2>
+<hr>
+
+- It is also known as one of the self-balancing trees. Purpose? => Avoid Skewdness
+- There is a balance (aka threshold) which will ensure that we don't have a skedness. If it's broken `=>` we'll have our tree rebalanced
+
+<ol>
+	<li>Balance</li>
+	<li>Rotations</li>
+	<li>Implementation</li>
+</ol>
+
+<hr>
+<h3>Balance</h4>
+
+
+- Let's imagine our tree:
+```bash
+	 n
+	/ \
+   Tl  Tr
+```
+- Then, we have a <b>height</b> of a tree.
+	+ it's 0 if we have only **root**
+	+ it's `-1 || null` if we doesn't have anything
+
+- Set of actions:
+	+ `H = max(H(Tl), H(Tr)) + 1`
+	+ `B(n) = H(Tl) - H(Tr)`. This is a formula for balance<br>
+		Further down we'll use `B` to say that we calc. balance, <b>simple height</b>
+	+ AVL Tree = |B(n)| <= 1 where || means `abs()` && 1 can be parameterized
+
+- Let's peruse the example below:
+	+ Example below is a balanced tree as: **B(3) = 1 - 0 => 1**
+	+ If `B` is positive => <i>left-heavy</i>
+	+ If `B` is negative => <i>right-heavy</i>
+		* It shows which part dominates the equation
+	+ B(0) = 0; B(2) = 0 hence B(1) is 0 **BUT** when we come back to 3,<br>
+		we add + 1 after 1, hence we have heigh of 1. Then, H(4) = -1 - (-1) = 0
+
+```bash
+		3
+	   / \
+	  1   4
+	 / \
+	0   2
+```
+
+- Now, let's peruse non-AVL tree:
+```bash
+		 4
+		/
+	   3
+	  /
+	 2
+	/ \
+   1   0
+```
+
+- After eyeboll, we come to the closing: 2 - (-1) = 2 + 1 = 3 => broken the **threshold** && it's not the AVL Tree.
+- And if we wanted to calc. the balance:
+	+ B(1) = -1 - (-1) = 0
+	+ B(0) = -1 - (-1) = 0
+	+ B(2) = 0 - 0 = 0. Then we add 1 if we calc. height (see above)
+	+ B(3) = 0 - (-1) = 1
+	+ B(4) = 1 - (-1) = 2
+
+<hr>
+<h3>Rotations</h4>
+
+<h5>Overview</h5>
+- It is how we'll maintain the <b>balance</b> aka fix imbalance
+
+- There are 2 so-called **heavy** situations:
+	* left-heavy: when left part is bigger:
+		* How to fix it?
+			<ol>
+				<li>right</li>
+				<li>left-right</li>
+			</ol>
+	* right-heavy: when right part is bigger
+		* How to fix it?
+			<ol>
+				<li>left</li>
+				<li>right-left</li>
+			</ol>
+	* There are always 2 nodes in a tangle. Look at the **screen 1**:
+		+ first one is before **rotation** && second one is after **rotation**
+		+ y < x
+		+ **red** < y && < x
+		+ **blue** >= y && < x
+		+ **purple** >= y && >= x
+
+<h5>1.1 Right rotation</h5>
+
+- Let's inser values 3,2,1
+	+ H(1): max(-1,-1) + 1 = 0
+	+ H(2): max(0,-1) + 1 => 1
+	+ H(3): max(1,-1) + 1 => 2
+- If we calc. simple Balance:
+	+ B(1) = -1 - (-1) = 0
+	+ B(2) = 0 - (-1) = 1
+	+ B(3) = 1 - (-1) = 2
+
+- We're <i>left-heavy</i> as we have positive integer
+- Start rotation:
+	+ focus only at **x** && **y**
+```bash
+	 3       2
+	/		/ \
+   2    => 1   3
+  /
+ 1
+```
+
+<h5>1.2 Left-right</h5>
+
+- Let's insert values 3,1,2
+	+ H(2): max(-1,-1) + 1 = 0
+	+ H(1): max(-1,0) + 1 = 1
+	+ H(3): max(1,-1) + 1 = 2
+
+- Now let's calc. Balance:
+	+ B(2) = -1 - (-1) = 0
+	+ B(1) = 0 - (-1) = 1
+	+ B(3) = 1 - (-1) = 2
+
+- If we do <i>right</i> rotation, then it'll give us skewed tree to the right
+
+```bash
+	3      1
+   / 		\
+  1   =>     3 - BAD as it has -2 overall balance
+   \		/
+    2	   2
+```
+
+- That's why we use **left-right** rotation where:
+	+ at first apply **left** to 1 && 2
+	+ then apply **right** to 2 && 3:
+
+```bash
+		3       3     2
+	   /       /     / \
+	  1   =>  2  => 1   3 
+	   \     /
+	    2   1
+```
+
+<h5>2.1 Left rotation</h5>
+
+- Let's insert values 1,2,3. Opposite of **screen1**
+	+ Again, focus only on **x** && **y** where former is 1 && latter is 2
+
+- If we calc **Balance**:
+	+ B(3) = -1 - (-1) = 0
+	+ B(2) = 0 - (-1) = 1
+	+ B(1) = -1 - 1 = -2
+
+- Let's calc. height before <i>rotation</i>:
+	+ H(3) = max(-1,-1) + 1 = 0
+	+ H(2) = max(0,-1) + 1 = 1
+	+ H(1) = max(1,-1) + 1 = 2
+
+```bash
+	1                2
+	 \              / \
+	  2      =>    1   3
+	   \
+	    3
+```
+
+<h5>2.2 Right-left</h5>
+
+- Let's insert the following: 1,3,2 && calc. balance before <i>rotations</i>
+	+ B(2) = -1 - (-1) = 0
+	+ B(3) = 0 - (-1) = 1
+	+ B(1) = -1 (left subtree) - 1 = -2
+
+- Height before rotation: 2
+
+```bash
+	1        1          2  
+	 \        \        / \
+	  3  =>    2   => 1   3
+	 /          \
+	2            3
+```
+
+<h4>Screenshots</h4>
+
+1. ![Alt text](img/first_screen.jpg?raw=true)
---
+++ AVL_trees/AVL_implementation.py
@@ -0,0 +1,203 @@
+"""
+1. In this file there is Python implementation
+of AVL tree.
+
+2. If you want to read the theory behind further
+implementation, check out `AVL.md`
+
+3. In this implementation `root` keeps `TreeNode`
+class, hence it's our AVL_Tree
+
+4. Important: don't forget that Rebalancing happens not only
+through the `root` of the whole tree, but through subtrees as well
+
+TODO: provide example
+"""
+from typing import Optional
+
+
+class TreeNode:
+    """
+    class that depicts the Node
+    """
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AvlTree:
+    """
+    class that depicts methods for AVL tree
+    """
+
+    def insert_node(self, root: Optional[TreeNode], new_node: int):
+        """
+        method that is responsible for inserting
+        new value in the tree
+
+        Steps:
+        1. insert in the correct position as LEAF node
+        2. update height
+        3. check balance
+        4. As we recursively enter the tree, we will
+        come back layer by layer updating height and checking
+        balance. Hence at one moment we can catch unbalanced stuff
+        """
+        if not root:
+            return TreeNode(new_node)
+        elif new_node >= root.value:
+            root.right = self.insert_node(root.right, new_node)
+        elif new_node < root.value: # else
+            root.left = self.insert_node(root.left, new_node)
+
+        root.height = 1 + max(
+            self._get_height(root.left),
+            self._get_height(root.right)
+        )
+
+        curr_balance = self._get_balance(root)
+
+        if curr_balance > 1: # left-heavy
+            if new_node > root.left.value:
+                root.left = self._left_rotate(root.left)
+                return self._right_rotate(root)
+            elif new_node < root.left.value:
+                return self._right_rotate(root)
+#       13
+#      /  \
+#     11   21
+#     /
+#    8
+#   /
+#  7  
+        if curr_balance < -1: # right-heavy
+            if new_node > root.right.value:
+                return self._left_rotate(root)
+            elif new_node < root.right.value:
+                root.right = self._right_rotate(root.right)
+                return self._left_rotate(root)
+
+        return root
+
+    def _get_height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def _get_balance(self, node):
+        if not node:
+            return 0
+
+        left_height = self._get_height(node.left)
+        right_height = self._get_height(node.right)
+
+        return left_height - right_height
+
+    def _left_rotate(self, node: TreeNode):
+        """
+        Read in `_right_rotation` as here the mirror situation
+        """
+        y_node = node.right
+        z_node = y_node.left
+
+        y_node.left = node
+        node.right = z_node
+
+        node.height = 1 + max(
+            self._get_height(node.left), self._get_height(node.right)
+        )
+        y_node.height = 1 + max(
+            self._get_height(y_node.left), self._get_height(y_node.right)
+        )
+
+        return y_node
+
+    def _right_rotate(self, node: TreeNode):
+        """
+        Look at the screenshot: 
+        - `node == x`
+        - `y_node == y`
+        - `z_node == right of y aka blue node` 
+        """
+
+        # gather nodes to perform rotation
+        y_node = node.left
+        z_node = y_node.right
+
+        # perofrm rotation
+        y_node.right = node
+        node.left = z_node
+
+        # update heights of our `x` & `y` nodes
+        node.height = 1 + max(
+            self._get_height(node.left), self._get_height(node.right)
+        )
+        y_node.height = 1 + max(
+            self._get_height(y_node.left), self._get_height(y_node.right)
+        )
+
+        # as `y_node` is our new `root` for the current subtree (or even whole tree)
+        return y_node
+
+    def delete_node(self, node: Optional[TreeNode], value: int):
+        if not node:
+            return node
+        
+        elif node.value < value:
+            node.right = self.delete_node(node.right, value)
+        elif node.value > value:
+            node.left = self.delete_node(node.left, value)
+        else:
+            if node.left is None:
+                temp_node_right = node.right
+                node = None
+                return temp_node_right
+            elif node.right is None:
+                temp_node_left = node.left
+                node = None
+                return temp_node_left
+            else:
+                temp_from_right_subtree = self._get_min_value_node(node.right)
+                node.value = temp_from_right_subtree.value
+                node.right = self.delete_node(node.right, temp_from_right_subtree.value)
+        
+        if node is None:
+            return node
+        
+        node.height = 1 + max(
+            self._get_height(node.left), self._get_height(node.right)
+        )
+
+        curr_balance = self._get_balance(node)
+
+        # if left-heavy
+        if curr_balance > 1 and self._get_balance(node.left) >= 0:
+            return self._right_rotate(node)
+        # if right-heavy
+        if curr_balance < -1 and self._get_balance(node.right) <= 0:
+            return self._left_rotate(node)
+        # if left-heavy and left sub-tree has right-heavy stuff
+        if curr_balance > 1 and self._get_balance(node.left) < 0:
+            node.left = self._left_rotate(node.left)
+            return self._right_rotate(node)
+        # if right-heavy and right sub-tree has left-heavy stuff
+        if curr_balance < -1 and self._get_balance(node.right) > 0:
+            node.right = self._right_rotate(node.right)
+            return self._left_rotate(node)
+
+        return node
+        
+    def _get_min_value_node(self, node):
+        if not node or node.left is None:
+            return node
+        return self._get_min_value_node(node.left)
+
+
+tree = AvlTree()
+nums = [33, 13, 52, 9, 21, 61, 8, 11]
+root = None
+
+for num in nums:
+    root = tree.insert_node(root, num)
